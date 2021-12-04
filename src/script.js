d7dbb806f6da5434ff20cdeb9abeab04e99d1f17
George <georgesucuzhanay100@gmail.com>
2021-12-03 23:02:23 -0500

    added api link to make calls

---
+++ src/script.js
@@ -0,0 +1,10 @@
+
+
+// api calls
+// https://www.affirmations.dev/ => return random affirmations
+// https://api.fisenko.net/v1/authors/en?query="Steve Jobs"&limit=50&offset=0   => return Jobs's ID
+// https://api.fisenko.net/v1/quotes/en/random/ => get random famous quotes
+// https://api.fisenko.net/v1/authors/en/6153b7d49e8e5ae3eb230a5b/quotes?query=&limit=1&offset=0 => returns Job's famous quote object
+
+//
+//
