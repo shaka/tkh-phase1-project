ae8ca0b2066d4dd167f6929bb257f53d123a1c87
Shaka P <shakalee@Shakas-MacBook-Pro-2.local>
2021-12-13 16:53:33 -0600

    update js

--- src/script.js
+++ src/script.js
@@ -1,21 +1,24 @@
-const url = 'https://www.affirmations.dev/';
+// using cors anywhere demo server
+// https://cors-anywhere.herokuapp.com/
+// need to enable it first by going to the link above
+const url = 'https://cors-anywhere.herokuapp.com/https://www.affirmations.dev';
 var headers = {}
 // The fetch() method returns a promise. If the promise returned is resolve, the function within the then() method is executed. That function contains the code for handling the data received from the API.
 fetch(url, {
-    method : "GET",
-    mode: 'cors',
-    headers: headers
+  headers: {
+    Accept: "application/json"
+  }
 })
-    //make output data come back in json format
-    .then((resp) => resp.json())
-    //console.log the advice quote from the data object 
-    .then(function(){
-        console.log(res);
-    })
-    // The API you call using fetch() may be down or other errors may occur. If this happens, the reject promise will be returned. The catch method is used to handle reject. The code within catch() will be executed if an error occurs when calling the API of your choice.
-    .catch(function(error) {
-        console.log(error)
-    });
+.then((resp) => resp.json())
+.then(result => {
+    const { affirmation } = result
+    const pTag = document.getElementById("inspiration-quote")
+    pTag.innerText = affirmation
+})
+.catch(function(error) {
+    console.error("Error Retrieving", error)
+});
+
 // api calls
 // https://www.affirmations.dev/ => return random affirmations
 // https://api.fisenko.net/v1/authors/en?query="Steve Jobs"&limit=50&offset=0   => return Jobs's ID
---
+++ src/searchName.js
@@ -0,0 +1,20 @@
+function searchName(event) {
+    // to prevent page refresh
+    event.preventDefault()
+    // const name = document.getElementById('lname').value
+    // const urlWithQuery = `https://api.fisenko.net/v1/authors/en?query="${name}"&limit=50&offset=0 `
+    // fetch(urlWithQuery, {
+    //     headers: {
+    //       Accept: "application/json"
+    //     }
+    //   })
+    //   .then((resp) => {
+    //       resp.json()
+    //   })
+    //   .then(result => {
+    //     console.log("searchName", result)
+    //   })
+    //   .catch(function(error) {
+    //       console.error("Error Retrieving", error)
+    //   });
+}
